e0da42d64d53e917bc91f9a60e8f7c91f424e1a2
William James Fawcett <william.fawcett@cern.ch>
2018-07-12 19:08:52 -0700

    adding modification of input files

--- pyatmos/simulation.py
+++ pyatmos/simulation.py
@@ -5,6 +5,10 @@ import os
 
 import pyatmos
 
+def print_list(li):
+    for e in li:
+        print(e.replace('\n',''))
+
 class Simulation():
     def __init__(self, docker_image='registry.gitlab.com/frontierdevelopmentlab/astrobiology/pyatmos'):
         self._docker_image = docker_image
@@ -18,21 +22,19 @@ class Simulation():
     def start(self):
         print('Starting Docker container...')
         self._container = self._docker_client.containers.run('registry.gitlab.com/frontierdevelopmentlab/astrobiology/pyatmos', detach=True, tty=True)
-        print('Container running.')
+        print('Container running {0}.'.format(self._container.name))
 
-    def run(self, species, max_iterations, n_clima_steps):
+    def run(self, species, max_photochem_iterations, n_clima_steps=400):
         '''
         Args: 
             species: dictionary, 
-            max_iterations: int, 
-            n_clima_steps: int, number of steps taken by clima 
+            max_photochem_iterations: int, maximum number of iterations allowed by photochem to test for convergence  
+            n_clima_steps: int, number of steps taken by clima (default 400) 
         '''
 
-        # modify species file
+        # modify species file: TODO
         species_file = self._read_container_file('/code/atmos/PHOTOCHEM/INPUTFILES/species.dat')
 
-        # overwrite a file 
-        print(type(species_file))
 
 
 
@@ -41,13 +43,25 @@ class Simulation():
         print('run photo finished')
 
         # check for convergence of photochem   
-        photochem_converged = self.check_photochem_convergence(max_iterations)
+        photochem_converged = self.check_photochem_convergence(max_photochem_iterations)
+
+        # copy photochem results
+        cmd = 'docker cp ' + self._container.name + ':/code/atmos/PHOTOCHEM/OUTPUT/out.out /Users/Will/Documents/FDL/results' 
+        os.system(cmd)
+        cmd = 'docker cp ' + self._container.name + ':/code/atmos/PHOTOCHEM/OUTPUT/out.dist /Users/Will/Documents/FDL/results' 
+        os.system(cmd)
+
+
+
+        ################################
+        # if photochem converged, run clima
+        ################################
 
         if photochem_converged: 
             print('photochem converged')
 
-            #Modify /CLIMA/IO/TEMPLATES/ModernEarth/input_clima.dat to /CLIMA/IO (this includes NSTEPS=    (number)) to change the number of steps
-            clima_input = self._read_container_file('/code/atmos/CLIMA/IO/TEMPLATES/ModernEarth/input_clima.dat')
+            # Modify CLIMA/IO/TEMPLATES/ModernEarth/input_clima.dat to change NSTEPS parameter 
+            clima_input = self._read_container_file('/code/atmos/CLIMA/IO/input_clima.dat')
             replacement_clima = [] 
             for line in clima_input:
                 if 'NSTEPS=' in line:
@@ -57,45 +71,47 @@ class Simulation():
             tmp_file = open(tmp_file_name, 'w')
             for l in replacement_clima:
                 tmp_file.write(l)
-            self._write_container_file(tmp_file, '/code/atmos/CLIMA/IO/TEMPLATES/ModernEarth/input_clima.dat')
+            tmp_file.close() # VERY important to close the file!! 
+            self._write_container_file(tmp_file_name, '/code/atmos/CLIMA/IO/input_clima.dat')
 
 
-            # check that it worked
-            new_clima = self._read_container_file('/code/atmos/CLIMA/IO/TEMPLATES/ModernEarth/input_clima.dat')
-
-
-
-
-
-
-
-
+            
+            ################################
             #To help with convergence, potentially replace /CLIMA/IO/TempIn.dat with /CLIMA/IO/TempOut.dat
             #Also set IUP=       0 in /CLIMA/IO/input_clima.dat
+            ################################
 
-            #if converged:
-            #./Clima.run
 
-            #
+            self._container.exec_run("cp  /code/atmos/CLIMA/IO/TempOut.dat /code/atmos/CLIMA/IO/TempIn.dat")
+            self._container.exec_run("sed -i 's/IUP=       1/IUP=       0/g' /code/atmos/CLIMA/IO/input_clima.dat")
 
-            print('running clima ...')
-            #self._container.exec_run('./Clima.run')
+
+            # Run clima 
+            print('running clima with {0} steps ...'.format(n_clima_steps))
+            self._container.exec_run('./Clima.run')
             print('finished clima')
+
+    
+            # copy clima output files 
+            cmd = 'docker cp ' + self._container.name + ':/code/atmos/CLIMA/IO/clima_allout.tab /Users/Will/Documents/FDL/results' 
+            os.system(cmd)
+            
+            
             return 1 
 
         else:
-            print('photochem did not converge before {0} iterations'.format(max_iterations))
+            print('photochem did not converge before {0} iterations'.format(max_photochem_iterations))
 
             return 1 
 
 
 
-    def check_photochem_convergence(self, max_iterations):
+    def check_photochem_convergence(self, max_photochem_iterations):
         '''
         Check that photochem has converged, search the output file for N = (number)
-        if number < max_iterations then convergence has been achived 
+        if number < max_photochem_iterations then convergence has been achived 
         Args:
-            max_iterations: an interger with the maximum number of iterations for convergence 
+            max_photochem_iterations: an interger with the maximum number of iterations for convergence 
         '''
 
         #output = self._container.exec_run("grep 'N =' /code/atmos/PHOTOCHEM/OUTPUT/out.out")
@@ -110,17 +126,18 @@ class Simulation():
         last_line = ' '.join(last_line.split()) # merge whitespace 
         number_of_iterations = int(last_line.split()[2])
 
-        if number_of_iterations < max_iterations:
+        if number_of_iterations < max_photochem_iterations:
             return True
         else:
             return False 
 
     def _write_container_file(self, input_file_name, output_file_name):
         cmd = 'docker cp ' + input_file_name + ' ' + self._container.name + ':' + output_file_name
+        os.system(cmd)
+
 
     def _read_container_file(self, container_file_name):
         tmp_file_name = tempfile.NamedTemporaryFile().name
-        print('_read_container_file: ' + tmp_file_name)
         cmd = 'docker cp ' + self._container.name + ':' + container_file_name + ' ' + tmp_file_name
         os.system(cmd)
         return pyatmos.util.strings_file(tmp_file_name)
--- pyatmos/util.py
+++ pyatmos/util.py
@@ -24,3 +24,54 @@ def strings_file(file_name):
             li.append(line)
     return li 
 
+
+def parse_species(file_name):
+    with open(file_name, 'r') as file:
+        ret = {}
+        for line in file.readlines():
+            if not (line.startswith('*')):
+                line = line.rstrip('\n\r')
+                if len(line) > 0:
+                    pass
+
+def species_header():
+    return '''
+***** SPECIES DEFINITIONS *****
+*
+*define LL,SL,TD, etc here
+*
+*LBOUND = lower boundary conditions
+* 0 = constant deposition velocity (VDEP)
+* 1 = constant mixing ratio
+* 2 = constant upward flux (SGFLUX)
+* 3 = constant vdep + vertically distributed upward flux  (uses SGFLUX and DISTH)
+*
+*MBOUND - Upper boundary conditions
+* 0 = CONSTANT EFFUSION VELOCITY (VEFF)  - (H and H2 set in code for molecular diffusion/diffusion limited flux)
+* 1 = constant mixing ratio - never been used so needs testing
+* 2 = CONSTANT FLUX (SMFLUX) (option for CO2/CO/0 in code)
+*
+*
+*   LONG-LIVED O H C S N CL LBOUND  VDEP0   FIXEDMR SGFLUX    DISTH MBOUND SMFLUX  VEFF0  '''
+
+def species_footer():
+    return '''
+* NQ should be the number above
+*   TRIDIAGONAL SOLVER
+*NQ1 should be the number directly above
+*   SHORT-LIVED SPECIES
+HNO2       SL  2 1 0 0 1 0
+O1D        SL  1 0 0 0 0 0
+CH21       SL  0 2 1 0 0 0
+CH23       SL  0 2 1 0 0 0
+C2H5       SL  0 5 2 0 0 0
+SO21       SL  2 0 0 1 0 0
+SO23       SL  2 0 0 1 0 0
+HSO3       SL  3 1 0 1 0 0
+OCS2       SL  1 0 1 2 0 0
+*   INERT SPECIES
+CO2        IN  2 0 1 0 0 0    3.6E-4       !must be second to last IN 
+N2         IN  0 0 0 0 2 0    0.78          !must be last IN  (FIXED MR NOT YET USED...)
+* NSP should be the number directly above
+HV         HV  0 0 0 0 0 0
+M          M   0 0 0 0 0 0'''
