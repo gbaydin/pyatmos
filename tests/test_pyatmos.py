9dcd3258abb3e750c83ca3269dd0ec6844e83c33
William James Fawcett <william.fawcett@cern.ch>
2018-08-01 15:43:30 -0700

    update testing script

--- tests/test_pyatmos.py
+++ tests/test_pyatmos.py
@@ -1,7 +1,10 @@
+import os
+THISDIR=os.path.dirname(os.path.abspath(__file__))
+
 import pyatmos
 
 atmos = pyatmos.Simulation(docker_image="gcr.io/i-agility-205814/pyatmos_docker",DEBUG=True)
 atmos.Start()
-state = atmos.run()
+state = atmos.run(output_directory=THISDIR+'/test_results')
 data = atmos.get_metadata() 
 #atmos.exit()
